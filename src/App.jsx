6fd28481938ed5ebaa0d819d69509855067d0d48
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2024-10-05 17:07:47 +0200

    Update Feedback function

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Description from "./components/App/Description/Description"
 import Options from "./components/App/Options/Options"
 import Feedback from "./components/App/Feedback/Feedback"
+import Notification from "./components/App/Notification/Notification"
 
 const App = () => {
   const [categories, setCategories] = useState(() => {
@@ -15,19 +16,23 @@ const App = () => {
   }, [categories])
 
   const totalFeedback = categories.good + categories.neutral + categories.bad;
-  const positiveFeedback = Math.round((categories.good / totalFeedback) * 100);
+  const positiveFeedback = Math.round((categories.good / totalFeedback) * 100); 
+
+  const onFeedbackButtons = (categoryName) => {
+    setCategories({ ...categories, [categoryName]: categories[categoryName] + 1})
+  }
   
-  const onGoodBtnClick = () => {
-    setCategories({ ...categories, good: categories.good + 1})
-  }
+  // const onGoodBtnClick = () => {
+  //   setCategories({ ...categories, good: categories.good + 1})
+  // }
 
-  const onNeutralBtnClick = () => {
-    setCategories({ ...categories, neutral: categories.neutral + 1})
-  }
+  // const onNeutralBtnClick = () => {
+  //   setCategories({ ...categories, neutral: categories.neutral + 1})
+  // }
 
-  const onBadBtnClick = () => {
-    setCategories({ ...categories, bad: categories.bad + 1})
-  }
+  // const onBadBtnClick = () => {
+  //   setCategories({ ...categories, bad: categories.bad + 1})
+  // }
 
   const onResetBtnClick = () => {
     setCategories({ good: 0, neutral: 0, bad: 0 })
@@ -37,9 +42,7 @@ const App = () => {
     <div>
       <Description />
       <Options
-        onGoodBtnClick={onGoodBtnClick}
-        onNeutralBtnClick={onNeutralBtnClick}
-        onBadBtnClick={onBadBtnClick}
+        onFeedbackButtons={onFeedbackButtons}
         onResetBtnClick={onResetBtnClick}
         total={totalFeedback}
       />
@@ -47,7 +50,7 @@ const App = () => {
         <Feedback
           categories={categories}
           total={totalFeedback}
-          positive={positiveFeedback}/> : "No feedback yet"}
+          positive = { (categories.good != 0) ? positiveFeedback : "0" } /> : <Notification/>}
     </div>
   )
 }
--- src/components/App/Options/Options.jsx
+++ src/components/App/Options/Options.jsx
@@ -1,11 +1,11 @@
 import styles from './Options.module.css'
 
-const Options = ({onGoodBtnClick, onNeutralBtnClick, onBadBtnClick, onResetBtnClick, total}) => {
+const Options = ({onFeedbackButtons, onResetBtnClick, total}) => {
   return (
     <ul className={styles.listOfButtons}>
-       <li><button onClick={onGoodBtnClick} type="button">Good</button></li> 
-       <li><button onClick={onNeutralBtnClick} type="button">Neutral</button></li> 
-       <li><button onClick={onBadBtnClick} type="button">Bad</button></li> 
+       <li><button onClick={() => onFeedbackButtons("good")} type="button">Good</button></li> 
+       <li><button onClick={() => onFeedbackButtons("neutral")} type="button">Neutral</button></li> 
+       <li><button onClick={() => onFeedbackButtons("bad")} type="button">Bad</button></li> 
        {(total != 0) && 
        <li><button onClick={onResetBtnClick} type="button">Reset</button></li>}
     </ul>
